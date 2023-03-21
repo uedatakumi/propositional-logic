3131e3b905c67ad61dc1b2bef2296e479225255d
Takumi Ueda <takumi@ueda.dev>
2023-03-21 14:09:58 +0900

    Add Rank.hs

---
+++ src/Rank.hs
@@ -0,0 +1,11 @@
+module Rank where
+
+import Prop
+
+rank :: Prop -> Int
+rank (Con _) = 0
+rank (Var _) = 0
+rank (Not p) = rank p + 1 
+rank (And p q) = maximum [rank p, rank q] + 1
+rank (Or  p q) = maximum [rank p, rank q] + 1
+rank (Imp p q) = maximum [rank p, rank q] + 1
